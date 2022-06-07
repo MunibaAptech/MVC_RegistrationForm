5639acc8733bb3256b42cc1e5b3ecdeff5ff3d2b
MunibaAptech <104563984+MunibaAptech@users.noreply.github.com>
2022-06-07 10:31:33 +0500

    all files update

---
+++ RegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(tbl_UserRegistration user)
+        {
+            WebAppEntities db = new WebAppEntities();
+            db.tbl_UserRegistration.Add(user);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
